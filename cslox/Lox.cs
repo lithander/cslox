84331578ba0f75163fabd8996d75cda2d04b5e0d
lithander <lithander@gmx.de>
2019-01-05 15:35:55 +0100

    Initial Commit - Chapter 4.1

---
+++ cslox/Lox.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cslox
+{
+    class Lox
+    {
+        //System Error Codes (https://docs.microsoft.com/de-de/windows/desktop/Debug/system-error-codes)
+        const int ERROR_BAD_ARGUMENTS = 0xA0;//160
+
+        static void Main(string[] args)
+        {
+            if(args.Length > 1)
+            {
+                Console.WriteLine("Usage: cslox [script]");
+                Environment.Exit(ERROR_BAD_ARGUMENTS);
+            }
+
+            if (args.Length == 1)
+                RunFile(args[0]);
+            else
+                RunPromt();
+        }
+
+        private static void RunPromt()
+        {
+            for(;;)
+            {
+                Console.Write("> ");
+                Run(Console.ReadLine());
+            }
+        }
+
+        private static void RunFile(string path)
+        {
+            Run(File.ReadAllText(path));
+        }
+
+        private static void Run(string script)
+        {
+            Console.WriteLine(script);
+        }
+    }
+}
---
+++ cslox/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("cslox")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("cslox")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("a34f7d16-9dcf-4a9e-ba04-4cb6f2c28353")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
